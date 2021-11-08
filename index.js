9c4b8a2cd1e0af107c12b3cfced22879f17c1a02
lreboseno22 <liamrebo22@gmail.com>
2021-11-08 15:23:49 +0000

    Challenge complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //write your code here
+function averageInArray(nums){
+    let average = 0;
+    for(let i = 0; i < nums.length; i++){
+        average += nums[i];
+    }
+    average / nums.length;
+    return average;
+}
+console.log(averageInArray([2, 2, 2, 2, 2]));
